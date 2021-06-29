da798fcabe2194ec34fb6b9465a6e026720628e9
Klint Youngmeyer <southqaw@gmail.com>
2021-06-29 13:28:15 -0500

    Add initial build scripts

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hatchrestbluepy",
+    version="0.0.1",
+    author="Klint Youngmeyer",
+    author_email="kkyoungmeyer@gmail.com",
+    description="A Python library to control the Hatch Rest sound machine, using bluepy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/southqaw/hatchrestbluepy",
+    project_urls={
+        "Bug Tracker": "https://github.com/southqaw/hatchrestbluepy/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Linux",
+        "Topic :: Home Automation",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
